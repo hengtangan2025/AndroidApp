418830a88787c901acce098f8dce58c57a1dc3ef
hengtangan2025 <504371515@qq.com>
2016-03-07 18:31:22 -0600

    0307 1831

---
+++ app/src/main/java/com/example/hengtangan2025/androidapp/PublishActivity.java
@@ -0,0 +1,82 @@
+package com.example.hengtangan2025.androidapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by hengtangan2025 on 2016/3/7.
+ */
+public class PublishActivity extends Activity {
+    public static final int TAKE_PHOTO = 1;
+    public static final int CROP_PHOTO = 2;
+    private Button takePhoto;
+    private ImageView picture;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+        takePhoto = (Button) findViewById(R.id.take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(Environment.getExternalStorageDirectory(),"output_image.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent("com.android.camera.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResualt(int requestCode, int resualtCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resualtCode == RESULT_OK) {
+                    Intent intent = new Intent ("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)；
+                    startActivityForResult(intent, CROP_PHOTO);
+                }
+                break;
+            case CROP_PHOTO
+                if (resualtCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
--- app/src/main/java/com/example/hengtangan2025/androidapp/SecondActivity.java
+++ app/src/main/java/com/example/hengtangan2025/androidapp/SecondActivity.java
@@ -2,7 +2,14 @@ package com.example.hengtangan2025.androidapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by hengtangan2025 on 2016/3/3.
@@ -14,5 +21,50 @@ public class SecondActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.second_layout);
+
+        Button Button2 = (Button) findViewById(R.id.button_2);
+        Button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               openOptionsMenu();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.second_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add:
+                break;
+            case R.id.menu_wallaper:
+                break;
+            case R.id.menu_search:
+                break;
+            case R.id.menu_setting:
+//                showSettings();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+//    private void showSettings(){
+//
+//        final Intent settings = new Intent(android.provider.Settings.ACTION_SETTINGS);
+//        settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+//                Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+//
+//        startActivity(settings);
+//    }
 }
+
+
+
+
